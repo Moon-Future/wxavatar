9745de542e3c83fe539a82dd817395897d34a4c4
liangchen25 <liangchen25@iflytek.com>
2021-12-06 18:39:15 +0800

    拖拽基础功能

---
+++ components/avatar/avatar.js
@@ -0,0 +1,123 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userInfo: {
+      type: Object,
+      default: {},
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    maskInfo: {
+      src: '/static/images/mask-01.png',
+      offsetTop: 0,
+      offsetLeft: 0,
+      top: 0,
+      left: 0,
+      x: 0,
+      y: 0,
+      scale: 1,
+      angle: 0,
+    },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    touchstart(e) {
+      const maskInfo = this.data.maskInfo
+      const { clientX, clientY } = e.touches[0]
+      this.startTouch = {
+        x: clientX, // 移动前坐标点
+        y: clientY,
+        angle: this.countDeg(maskInfo.x, maskInfo.y, clientX, clientY), // 移动前位置的角度
+        r: this.getDistance(maskInfo.x, maskInfo.y, maskInfo.offsetLeft, maskInfo.offsetTop), // 半径
+      }
+    },
+    touchmove(e) {
+      const type = e.currentTarget.dataset.type
+      const maskInfo = this.data.maskInfo
+      const { clientX, clientY } = e.touches[0]
+      this.moveTouch = {
+        x: clientX,
+        y: clientY,
+        angle: this.countDeg(maskInfo.x, maskInfo.y, clientX, clientY), // 移动后位置的角度
+        dis: this.getDistance(maskInfo.x, maskInfo.y, clientX, clientY), // 移动的点到圆心的距离
+      }
+      if (type === 'scale') {
+        // 缩放
+        maskInfo.scale = this.moveTouch.dis / this.startTouch.r
+      } else if (type === 'rotate') {
+        // 旋转
+        maskInfo.angle += this.moveTouch.angle - this.startTouch.angle 
+        this.startTouch.angle = this.moveTouch.angle
+      } else if (type === 'drag') {
+        maskInfo.left += this.moveTouch.x - this.startTouch.x
+        maskInfo.top += this.moveTouch.y - this.startTouch.y
+        maskInfo.x += this.moveTouch.x - this.startTouch.x
+        maskInfo.y += this.moveTouch.y - this.startTouch.y
+        this.startTouch.x = clientX
+        this.startTouch.y = clientY
+      }
+      
+      this.setData({
+        maskInfo,
+      })
+    },
+    touchend(e) {},
+    getDistance(cx, cy, pointer_x, pointer_y) {
+      var ox = pointer_x - cx
+      var oy = pointer_y - cy
+      return Math.sqrt(ox * ox + oy * oy)
+    },
+    /*
+     *参数1和2为图片圆心坐标
+     *参数3和4为手点击的坐标
+     *返回值为手点击的坐标到圆心的角度
+     */
+    countDeg: function (cx, cy, pointer_x, pointer_y) {
+      var ox = pointer_x - cx
+      var oy = pointer_y - cy
+      var to = Math.abs(ox / oy)
+      var angle = (Math.atan(to) / (2 * Math.PI)) * 360
+      // console.log("ox.oy:", ox, oy)
+      if (ox < 0 && oy < 0) {
+        //相对在左上角，第四象限，js中坐标系是从左上角开始的，这里的象限是正常坐标系
+        angle = -angle
+      } else if (ox <= 0 && oy >= 0) {
+        //左下角,3象限
+        angle = -(180 - angle)
+      } else if (ox > 0 && oy < 0) {
+        //右上角，1象限
+        angle = angle
+      } else if (ox > 0 && oy > 0) {
+        //右下角，2象限
+        angle = 180 - angle
+      }
+      return angle
+    },
+    imgOnLoad(e) {
+      const query = wx.createSelectorQuery().in(this)
+      query
+        .select('#maskImage')
+        .boundingClientRect((rect) => {
+          const maskInfo = this.data.maskInfo
+          maskInfo.x = rect.left + rect.width / 2
+          maskInfo.y = rect.top + rect.height / 2
+          maskInfo.offsetTop = rect.top
+          maskInfo.offsetLeft = rect.left
+          this.setData({ maskInfo })
+        })
+        .exec()
+    },
+  },
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,76 +1,39 @@
-// pages/home/home.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    navList: [
-      {
-        title: '节日头像',
-        descr: '一键加节日头像',
-        icon: ''
-      },
-      {
-        title: '节日头像',
-        descr: '一键加节日头像',
-        icon: ''
-      },
-      {
-        title: '节日头像',
-        descr: '一键加节日头像',
-        icon: ''
-      }
-    ]
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
+    CustomBar: app.globalData.CustomBar,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
   },
 
   /**
